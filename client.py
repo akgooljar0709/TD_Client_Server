d4c5dc5137cb455b3f92f04c4ba9c23b370bfd8d
akgooljar0709 <akgooljar@student.udm.ac.mu>
2022-03-17 17:37:23 +0400

    [Update Code]

--- client.py
+++ client.py
@@ -1,48 +1,86 @@
 import socket
-import argparse
+import os
 
- 
+import numpy as np
+from PIL import Image
+
+class Client:
+    def __init__(self):
+
+        print(self)
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.connect_to_server()
+
+    def connect_to_server(self):
+        self.target_ip = input('Enter ip --> ')
+        self.target_port = input('Enter port --> ')
+
+        self.s.connect((self.target_ip,int(self.target_port)))
+
+        self.main()
+
+    def reconnect(self):
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.s.connect((self.target_ip,int(self.target_port)))
 
 
-parser = argparse.ArgumentParser(description="Simple File image import")
-    
-parser.add_argument("filen", help="Image file name")
+    def main(self):
+        while 1:
+            file_name = input('Enter file name on server --> ')
 
-
-args = parser.parse_args()
-    
-filen = args.filen
-
-
-msgFromClient       = filen # image name - for example image.jpeg 
-
-bytesToSend         = str.encode(msgFromClient)
-
-serverAddressPort   = ("10.27.0.42", 12345)
-
-bufferSize          = 1024
-
- 
-
-# Create a UDP socket at client side
-
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
- 
-
-# Send to server using created UDP socket
-
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-
- 
-
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-
- 
-
-msg = "Message from Server {}".format(msgFromServer[0])
-
-print(msg)
-
+            
 
     
+
+            self.s.send(file_name.encode())
+
+            confirmation = self.s.recv(1024)
+            if confirmation.decode() == "file-doesn't-exist":
+                print("File doesn't exist on server.")
+
+                self.s.shutdown(socket.SHUT_RDWR)
+                self.s.close()
+                self.reconnect()
+
+            else:        
+                write_name = 'from_server_'+file_name
+
+
+                if os.path.exists(write_name): os.remove(write_name)
+
+                with open(write_name,'wb') as file:
+                    while 1:
+                        data = self.s.recv(1024)
+
+                        if not data:
+                            break
+
+                        file.write(data)
+
+                print(file_name,'successfully downloaded.')
+
+                img_data = Image.open(write_name)
+                img_arr = np.array(img_data)
+
+                z =  len(img_arr)
+
+                w = bin(z)
+
+                print(w)
+
+            
+                
+
+                for i in img_arr.shape[1::-1]:
+
+                    if(i%2 == 0):
+                        print("pair")
+
+                    else:
+                        print("impair")
+
+                self.s.shutdown(socket.SHUT_RDWR)
+                self.s.close()
+                self.reconnect()
+                
+client = Client()
--- server.py
+++ server.py
@@ -1,84 +1,82 @@
 import socket
+import threading
+import os
 import numpy as np
 from PIL import Image
 
- 
+class Server:
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.accept_connections()
+    
+    def accept_connections(self):
+        ip = '10.27.0.42'
+        print (ip)
+        port = int(input('Enter desired port --> '))
 
-localIP     = "10.27.0.42"
+        self.s.bind((ip,port))
+        self.s.listen(100)
 
-localPort   = 12345
+        print('Running on IP: '+ip)
+        print('Running on port: '+str(port))
 
-bufferSize  = 1024
-
- 
-
-
-
- 
-
-# Create a datagram socket
-
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
- 
-
-# Bind to address and ip
-
-UDPServerSocket.bind((localIP, localPort))
-
- 
-
-print("UDP server up and listening")
-
- 
-
-# Listen for incoming datagrams
-
-while(True):
-
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-
-    message = bytesAddressPair[0]
-
-    address = bytesAddressPair[1]
-
-    img_data = Image.open(message)
-    img_arr = np.array(img_data)
-
-    z =  len(img_arr)
+        while 1:
+            c, addr = self.s.accept()
+            print(c)
+            
+            threading.Thread(target=self.handle_client,args=(c,addr,)).start()
 
 
-    for i in img_arr.shape[1::-1]:
-     # img.shape -> (width,height,channel)
 
-        if(i%2 == 0):
-            msgFromServer= "pair"
+    def handle_client(self,c,addr):
+        data = c.recv(1024).decode()
+        print(data)
+
+
+
+        img_data = Image.open(data)
+        img_arr = np.array(img_data)
+
+
+
+        h,w,_= img_arr.shape # Get the width and heigth of the image
+
+        print(h)
+        print(w)
+
+
+
+        z =  len(img_arr)
+        bin_rep=bin(z)
+
+        print(bin_rep)
+
+        for i in img_arr.shape[1::-1]: # img.shape -> (width,height,channel)
+
+            if(i%2 == 0):
+                    
+                print("pair")
+
+            else:
+
+                print("impair")
+
+    
+        if not os.path.exists(data):
+            c.send("file-doesn't-exist".encode())
 
         else:
+            c.send("file-exists".encode())
+            print('Sending',data)
+            if data != '':
+                file = open(data,'rb')
+                data = file.read(1024)
+                while data:
+                    c.send(data)
+                    data = file.read(1024)
 
-            msgFromServer= "impair"
+                c.shutdown(socket.SHUT_RDWR)
+                c.close()
+                
 
-
-    bytesToSend = str.encode(msgFromServer)
-
-
-    
-
-
-
-
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
-    
-    print(clientMsg)
-    print(clientIP)
-
-   
-
-    # Sending a reply to client
-
-    UDPServerSocket.sendto(bytesToSend, address)
-    
-    
-    
-    
+server = Server()
